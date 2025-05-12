47d1e440ea717ca87795aa94f51a2ae97006e5d4
Aseke <a.dzhakipbekova@mail.ru>
2025-05-12 23:03:43 +0500

    make payment

--- products/forms.py
+++ products/forms.py
@@ -1,15 +1,21 @@
 from django import forms
 from .models import CustomerAddress
 
-
 class CustomerAddressForm(forms.ModelForm):
     class Meta:
         model = CustomerAddress
-        fields = ['state','city','zip_code','street_address','mobile']
+        fields = ['state', 'city', 'zip_code', 'street_address', 'mobile']
+        labels = {
+            'state': 'Region',
+            'city': 'City',
+            'zip_code': 'Postal Code',
+            'street_address': 'Street Address',
+            'mobile': 'Mobile Number',
+        }
         widgets = {
-            'state': forms.TextInput(attrs={'class':'form-control'}),
-            'city': forms.TextInput(attrs={'class':'form-control'}),
-            'zip_code': forms.NumberInput(attrs={'class':'form-control'}),
-            'street_address': forms.Textarea(attrs={'class':'form-control','rows':5,'cols':50}),
-            'mobile': forms.NumberInput(attrs={'class':'form-control'}),
-        }        
+            'state': forms.TextInput(attrs={'class': 'form-control'}),
+            'city': forms.TextInput(attrs={'class': 'form-control'}),
+            'zip_code': forms.NumberInput(attrs={'class': 'form-control'}),
+            'street_address': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'cols': 50}),
+            'mobile': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
