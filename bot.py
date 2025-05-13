930f07ba602b8023e3cfe3f44d0a8a11f226fe16
Aseke <a.dzhakipbekova@mail.ru>
2025-05-13 11:47:44 +0500

    посл изм

--- bot.py
+++ bot.py
@@ -1,27 +1,26 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 TOKEN = "7581746741:AAFK9tL8BvgzJ8HbAV1CkBHzmdfOm4OAOzg"
 
-def start(update, context):
-    update.message.reply_text("🚀 Бот запущен! /help")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Я живой бот. Напиши мне что-нибудь")
 
-def help(update, context):
-    update.message.reply_text("Справка:\n/start\n/help")
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_text = update.message.text
+    await update.message.reply_text(f"🔹 Вы сказали: {user_text}\n"
+                                    f"🔹 Длина: {len(user_text)} символов")
 
-def unknown(update, context):
-    update.message.reply_text("Неизвестная команда 😢")
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f"Ошибка: {context.error}")
 
-def main():
-    updater = Updater(TOKEN)
-    dispatcher = updater.dispatcher
+if __name__ == "__main__":  # Исправлено
+    print("Запуск бота...")
+    app = Application.builder().token(TOKEN).build()
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help))
-    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.add_error_handler(error)
 
-    print("Бот работает! 🌍")
-    updater.start_polling()
-    updater.idle()
-
-if name == "main":
-    main()
+    print("Бот работает!")
+    app.run_polling()
